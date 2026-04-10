c13ee5775862094aa7cb981fda3f99f7e6b1c2f4
Christian <christian@katafract.com>
2026-04-10 19:09:12 +0000

    Fix: re-add background import removed by mistake

--- app/src/main/java/com/katafract/exifarmor/ui/PreviewScreen.kt
+++ app/src/main/java/com/katafract/exifarmor/ui/PreviewScreen.kt
@@ -1,5 +1,6 @@
 package com.katafract.exifarmor.ui
 
+import androidx.compose.foundation.background
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
